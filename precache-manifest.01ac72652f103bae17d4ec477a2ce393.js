2b09738654ffa23e833aa1db2720c9471299a3b1
nishan-iicm <nishan.iicm@gmail.com>
2021-03-13 20:34:14 +0530

    Updates

---
+++ precache-manifest.01ac72652f103bae17d4ec477a2ce393.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b593500e94b9150d655d3eee14b15b",
+    "url": "/GetGithubInfo/index.html"
+  },
+  {
+    "revision": "2c885734e87a2b0cde07",
+    "url": "/GetGithubInfo/static/css/main.cac136dc.chunk.css"
+  },
+  {
+    "revision": "d63c217b6a82a1d53177",
+    "url": "/GetGithubInfo/static/js/2.b9123421.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GetGithubInfo/static/js/2.b9123421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c885734e87a2b0cde07",
+    "url": "/GetGithubInfo/static/js/main.298214f3.chunk.js"
+  },
+  {
+    "revision": "011894e79c1b6378e62b",
+    "url": "/GetGithubInfo/static/js/runtime-main.ff51053a.js"
+  },
+  {
+    "revision": "d617763fd5c08d9196374c29974e30ab",
+    "url": "/GetGithubInfo/static/media/octocat.d617763f.svg"
+  }
+]);
